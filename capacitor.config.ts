c2b70107b61f6e44ee4c048c21e9d3075d987705
Nono0029 <noabelarbi@icloud.com>
2026-05-14 19:07:57 +0200

    Ajout SEO, Google Search Console, et Capacitor iOS

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.askoo.app',
+  appName: 'askoo',
+  webDir: 'dist'
+};
+
+export default config;
